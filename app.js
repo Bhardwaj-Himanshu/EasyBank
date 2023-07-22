6757e11abb5264a7bb028dd90eeed5aa1b4a0736
Himanshu Bhardwaj <himanshujay261201@gmail.com>
2023-07-22 21:03:29 +0530

    Animated the hamburger menu-just the x!

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('jehlo world!')
+const btnhamburger=document.querySelector('#btnhamburger');
+
+btnhamburger.addEventListener('click',function(){
+    
+    if (btnhamburger.classList.contains('open')){
+        console.log('Band ho ja sim sim!')
+        btnhamburger.classList.remove('open');
+    }
+    else{
+        console.log('Khul ja sim sim!')
+        btnhamburger.classList.add('open');
+    }
+}); 
